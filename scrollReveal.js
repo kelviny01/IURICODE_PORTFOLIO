6f93bc822375bf0ea1336bdc0ef28da1c0c32bfb
kelviny01 <kelviny.mk27@gmail.com>
2023-01-28 13:40:41 -0300

    inital commit

---
+++ scrollReveal.js
@@ -0,0 +1,107 @@
+window.sr = ScrollReveal({ reset: true });
+
+ScrollReveal().reveal(".card-1", {
+  delay: 600,
+  rotate: {
+    x: 100,
+    y: 0,
+    z: 0,
+  },
+});
+ScrollReveal().reveal(".card-2", {
+  delay: 600,
+  rotate: {
+    x: 100,
+    y: 0,
+    z: 0,
+  },
+});
+ScrollReveal().reveal(".card-3", {
+  delay: 600,
+  rotate: {
+    x: 100,
+    y: 0,
+    z: 0,
+  },
+});
+
+ScrollReveal().reveal(".infos-card", {
+  delay: 600,
+  rotate: {
+    x: 200,
+    y: 0,
+    z: 0,
+  },
+});
+
+ScrollReveal().reveal(".img-html", {
+  delay: 600,
+  rotate: {
+    x: 200,
+    y: 50,
+    z: 0,
+  },
+});
+
+ScrollReveal().reveal(".img-css", {
+    delay: 510,
+    rotate: {
+      x: 540,
+      y: 50,
+      z: 0,
+    },
+  });
+
+ScrollReveal().reveal(".img-js", {
+  delay: 510,
+  rotate: {
+    x: 200,
+    y: 50,
+    z: 0,
+  },
+});
+
+ScrollReveal().reveal(".img-sass", {
+  delay: 470,
+  rotate: {
+    x: 200,
+    y: 50,
+    z: 0,
+  },
+});
+
+ScrollReveal().reveal(".img-react", {
+  delay: 440,
+  rotate: {
+    x: 200,
+    y: 50,
+    z: 0,
+  },
+});
+
+ScrollReveal().reveal(".img-next", {
+  delay: 410,
+  rotate: {
+    x: 200,
+    y: 50,
+    z: 0,
+  },
+});
+
+ScrollReveal().reveal(".img-styled", {
+  delay: 330,
+  rotate: {
+    x: 200,
+    y: 50,
+    z: 0,
+  },
+});
+
+ScrollReveal().reveal(".img-tailwind", {
+  delay: 300,
+  rotate: {
+    x: 200,
+    y: 50,
+    z: 0,
+  },
+});
